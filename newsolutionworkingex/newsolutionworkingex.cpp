a66f36e8f321a02463e4d89115189da8d4e74c8c
akodrem2 <akodrem10@gmail.com>
2024-06-02 12:34:02 +0300

    commit1

--- newsolutionworkingex/newsolutionworkingex.cpp
+++ newsolutionworkingex/newsolutionworkingex.cpp
@@ -1,20 +1,23 @@
-// newsolutionworkingex.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-
+#include <vector>
+using namespace std;
+class point {
+    int x, y;
+public:
+    point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    point() {
+        x = y = 0;
+    }
+};
+class figura {
+    vector<point> vectors;
+    
+};
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
